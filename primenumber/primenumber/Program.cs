ee5de579241c3a21c58265ca003c03f9533afa61
Seifeslam77 <121711254+Seifeslam77@users.noreply.github.com>
2022-12-31 10:23:58 +0200

    Add files via upload

---
+++ primenumber/primenumber/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter the first num");
+int first_num = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second num");
+int second_num = int.Parse(Console.ReadLine());
+
+for (int i = first_num; i < second_num; i++)
+{
+    bool isprime = true;
+    for (int j = 2; j <= i / 2; j++)
+    {
+        if ((i % j) == 0)
+        {
+            isprime = false;
+            break;
+        }
+    }
+    if (isprime && i !=0 && i != 1)
+        Console.WriteLine(i);
+}
